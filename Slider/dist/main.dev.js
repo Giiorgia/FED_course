0d0fdaa967d2eaaebe15cb52a19c411ac67f8ae2
Giiorgia <giiorgiana.opreaa98@gmail.com>
2024-10-08 20:05:04 +0300

    Added new HW

---
+++ Slider/dist/main.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    //dots: true,
+    prevArrow: '<button type="button" class="nav-button slick-prev"></button>',
+    nextArrow: '<button type="button" class="nav-button slick-next"></button>'
+  });
+});
