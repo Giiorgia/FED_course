31c7313f5c8aed3ff11e96a2b8308c6863deb8fd
Giiorgia <giiorgiana.opreaa98@gmail.com>
2024-10-18 19:43:40 +0300

    added slider

---
+++ Project_2/dist/main.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.my_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true
+  });
+});
---
+++ Project_2/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.my_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+    });
+  });
