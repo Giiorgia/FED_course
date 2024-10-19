5fc5b9eaf467b6e892497a9adcd825a9e672e207
Giiorgia <giiorgiana.opreaa98@gmail.com>
2024-10-19 19:39:19 +0300

    added map and footer section

--- Project_2/js/dist/main.dev.js
+++ Project_2/js/dist/main.dev.js
@@ -15,3 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     speed: 500
   });
 });
+var map = L.map('map').setView([40.668391, -73.920258], 13);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png	', {
+  maxZoom: 19
+}).addTo(map);
